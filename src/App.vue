87a3c13a112bc7f547bbda781a35e8fa147d6634
sundlee <sundlee@ncsoft.com>
2022-03-29 09:37:58 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <fetch-data url="https://jsonplaceholder.typicode.com/users/1">
+      <template v-slot="{ response, loading }">
+        <ul v-if="!loading">
+          <li>Name: {{response.name}}</li>
+          <li>Mail: {{response.email}}</li>
+        </ul>
+        <div v-if="loading">
+          loading...
+        </div>
+      </template>
+    </fetch-data>
+  </div>
+</template>
+
+<script>
+import FetchData from './components/FetchData.vue'
+
+export default {
+  name: 'App',
+  components: {
+    FetchData,
+  },
+}
+</script>
